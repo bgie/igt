8e9638473ee752528e8a5c6d78918ce4707356b6
Brecht Kuppens <brecht@thinkpad-x1-bk.keyww.com>
2023-10-17 06:50:54 +0200

    Improved nodes and scenes tutorial +added some

--- addons/igt/topics/basics/NodesAndScenes.gd
+++ addons/igt/topics/basics/NodesAndScenes.gd
@@ -7,34 +7,36 @@ func _init():
 			"Hi!  In this topic we will learn about nodes and scenes.\n\nPress 'Next' to continue.",
 			null, _show_2D_editor),
 		TutorialStep.new("What are nodes?", 
-			"Nodes are the building blocks of your game.\n\nThere are nodes for graphics, sound, user interfaces, physics..."),
-		TutorialStep.new("Parent and child nodes", 
-			"Nodes are organized in a tree.\n\nA node can have multiple child nodes.\nA child can only belong to one parent.",
-			preload("res://addons/igt/topics/basics/parent_child_tree.svg")),
-		TutorialStep.new("Grandchild nodes", 
-			"Child nodes can have children of their own.\n\nYou can go as deep as you need.",
-			preload("res://addons/igt/topics/basics/parent_child_child_tree.svg")),
-		TutorialStep.new("Scene", 
-			"A tree of nodes can be saved as a scene.\n\nA scene has 1 parent node, also called the root node.\n\nYou will usually have multiple scenes in your game.",
-			preload("res://addons/igt/topics/basics/scene_with_tree.svg")),
-		TutorialStep.new("Reusing scenes", 
-			"Scenes can be reused.\n\nScenes do not only refer to backgrounds or game levels,\nbut scenes can be used for enemies, the player, pickups...\nor also user interface elements like dialogs."),
+			"Nodes are the building blocks of your game.\n\nThere are nodes for graphics, sound, user interfaces, physics, A.I. ..."),
+		TutorialStep.new("What is a scene? (1)", 
+			"Nodes can be grouped into a scene.\n\nScenes are reusable, and you will usually have multiple scenes in your game."),
+		TutorialStep.new("What is a scene? (2)", 
+			"The name 'scene' refers to a scene on stage, but scenes are used for more than that:\n\nNot only backgrounds and game levels, but also enemies, pickups, players, \nor user interface elements like dialogs."),
 		TutorialStep.new("Create a scene", 
 			"Lets create a new scene.\n\nSelect 'New Scene' from the 'Scene' menu.",
 			null, Callable(),
 			"Panel:0/VBoxContainer:0/EditorTitleBar:0/MenuBar:0",
 			_has_user_created_new_scene),
 		TutorialStep.new("Set a root node", 
-			"Excellent!\n\nNow we have to choose which kind of root node we want.\nThe available types will be explained later.\n\nSelect 'User Interface' (Control) for this excercise.",
+			"Excellent!\n\nNow we have to choose our first node. The available types will be explained later.\n\nSelect 'User Interface' (Control) for this excercise.",
 			null, Callable(),
 			"Panel:0/VBoxContainer:0/HSplitContainer:0/HSplitContainer:0/VSplitContainer:0/TabContainer:0/SceneTreeDock:0/VBoxContainer:0/ScrollContainer:0/VBoxContainer:0/VBoxContainer:0/Button:2",
 			_has_user_select_control_as_root),
 		TutorialStep.new("Control node", 
-			"Our new scene now has a root node of type 'Control'.\n\nControl is a base type for user interface elements.\nIt does not display anything itself, but can be used as a container or placeholder.",
+			"Our new scene now has a root node of type 'Control'.\n\nControl is a base type for user interface elements.\nIt is invisible in the game, but can be used as a container or placeholder.",
 			null, Callable(),
 			"Panel:0/VBoxContainer:0/HSplitContainer:0/HSplitContainer:0/VSplitContainer:0/TabContainer:0/SceneTreeDock:0/SceneTreeEditor:0/Tree:0"),
+		TutorialStep.new("Root node", 
+			"All the nodes in a scene will be organized in a tree.\n\nA scene always has 1 top-level node, called the root node.\n\nThe first node we add automatically becomes the root node.",
+			preload("res://addons/igt/topics/basics/scene_with_tree.svg")),
+		TutorialStep.new("Parent and child nodes", 
+			"Nodes in a tree are also called parent and child nodes.\n\nA parent node can have multiple child nodes.\nA child can only belong to one parent.",
+			preload("res://addons/igt/topics/basics/parent_child_tree.svg")),
+		TutorialStep.new("Grandchild nodes", 
+			"Child nodes can have children of their own.\n\nYou can go as deep as you need.\n\nThe top-most parent node is, you guessed it, the root node.",
+			preload("res://addons/igt/topics/basics/parent_child_child_tree.svg")),
 		TutorialStep.new("Add child node",
-			"Next we will add a child node.\n\nClick the 'Add Child Node' button.",
+			"We will add a child node now.\n\nClick the 'Add Child Node' button.",
 			null, Callable(),
 			"Panel:0/VBoxContainer:0/HSplitContainer:0/HSplitContainer:0/VSplitContainer:0/TabContainer:0/SceneTreeDock:0/HBoxContainer:0/Button:0",
 			_is_add_node_dialog_visible),
@@ -54,7 +56,6 @@ func _init():
 		TutorialStep.new("Switch to select mode", 
 			"The 2D view also allows you to edit the position and size of nodes.\n\nLets try this!\nFirst, make sure you are in the 'Select' mode.",
 			null, Callable(),
-# "Control:0/Panel:0/VBoxContainer:0/HSplitContainer:0/HSplitContainer:0/HSplitContainer:0/VBoxContainer:0/VSplitContainer:0/VSplitContainer:0/VBoxContainer:0/PanelContainer:1/VBoxContainer:0/CanvasItemEditor:0/HFlowContainer:0/HBoxContainer:0/Button:0"
 			"Panel:0/VBoxContainer:0/HSplitContainer:0/HSplitContainer:0/HSplitContainer:0/VBoxContainer:0/VSplitContainer:0/VSplitContainer:0/VBoxContainer:0/PanelContainer:1/VBoxContainer:0/CanvasItemEditor:0/HFlowContainer:0/HBoxContainer:0/Button:0"),
 		TutorialStep.new("Resize node", 
 			"To resize, simply use the round handles and drag them with the mouse.\n\nMake our ColorRect twice as large.",
@@ -64,7 +65,17 @@ func _init():
 			"Great!\n\nNow move the node to the right.\nThis is done by clicking inside the node and dragging it.",
 			preload("res://addons/igt/topics/basics/color_rect_handles_move.png"), Callable(), "",
 			_color_rect_was_moved),
-	
+		TutorialStep.new("Add second child node",
+			"Lets add another child node.\n\nClick the 'Add Child Node' button again.",
+			null, Callable(),
+			"Panel:0/VBoxContainer:0/HSplitContainer:0/HSplitContainer:0/VSplitContainer:0/TabContainer:0/SceneTreeDock:0/HBoxContainer:0/Button:0",
+			_is_add_node_dialog_visible),
+		TutorialStep.new("Add second ColorRect", 
+			"We want another colored rectangle.\n\nThis time, the type 'ColorRect' is available in the 'Recently Used' list.\n\nClick it and then click 'Create' to confirm.",
+			null, Callable(),
+			"Panel:0/VBoxContainer:0/HSplitContainer:0/HSplitContainer:0/VSplitContainer:0/TabContainer:0/SceneTreeDock:0/CreateDialog:0/HSplitContainer:0/VSplitContainer:0/VBoxContainer:1/MarginContainer:0/ItemList:0",
+			_has_added_grandchild_colorRect_node),
+
 		TutorialStep.new("Under construction", 
 			"This tutorial is not finished yet.\n\nYou have reached the end.",
 			preload("res://addons/igt/topics/common/under_construction.png")),
@@ -98,7 +109,16 @@ func _is_add_node_dialog_visible() -> bool:
 	return false
 
 func _has_added_child_colorRect_node() -> bool:
-	return EditorScript.new().get_editor_interface().get_edited_scene_root().get_children().any(func(node): return node is ColorRect)
+	return EditorScript.new().get_editor_interface().get_edited_scene_root().get_children().any(find_colorRect_and_set_color)
+
+func find_colorRect_and_set_color(node) -> bool:
+	if node is ColorRect:
+		node.color = Color("C0C0C0")
+		return true
+	return false
+
+func _has_added_grandchild_colorRect_node() -> bool:
+	return EditorScript.new().get_editor_interface().get_edited_scene_root().get_children().any(func(node): return node is ColorRect and node.get_children().any(func(grandchild): return grandchild is ColorRect))
 
 func _color_rect_was_made_larger() -> bool:
 	return EditorScript.new().get_editor_interface().get_edited_scene_root().get_children().any(func(node): return node is ColorRect and (node.size.x > 80 and node.size.y > 80))
