bc1ea80384e15a3218a17706beaf5f67d27c8d07
Brecht Kuppens <brecht@thinkpad-x1-bk.keyww.com>
2023-11-19 19:08:54 +0100

    Continued basic 2d nodes topic

--- addons/igt/topics/basics/Basic2dNodes.gd
+++ addons/igt/topics/basics/Basic2dNodes.gd
@@ -40,7 +40,7 @@ func _init():
 			null, Callable(),
 			"Panel:0/VBoxContainer:0/HSplitContainer:0/HSplitContainer:0/VSplitContainer:0/TabContainer:0/SceneTreeDock:0/SceneTreeEditor:0/Tree:0", false),
 		TutorialStep.new("Volgorde sprites (2)", 
-			"Is een knoop een kind van een andere knoop, dan wordt het kind later getekend, en komt dus bovenop in de 2D view.\n(Er is een optie om dit aan te passen, CanvasItem -> Visibility -> Show Behind Parent)",
+			"Is een knoop een kind van een andere knoop, dan wordt het kind later getekend, en komt dus bovenop in de 2D view.\n\n(Er is een optie om dit aan te passen, CanvasItem -> Visibility -> Show Behind Parent)",
 			null, Callable(),
 			"Panel:0/VBoxContainer:0/HSplitContainer:0/HSplitContainer:0/VSplitContainer:0/TabContainer:0/SceneTreeDock:0/SceneTreeEditor:0/Tree:0", false),
 		TutorialStep.new("Volgorde sprites (3)", 
@@ -48,7 +48,52 @@ func _init():
 			null, Callable(),
 			"Panel:0/VBoxContainer:0/HSplitContainer:0/HSplitContainer:0/VSplitContainer:0/TabContainer:0/SceneTreeDock:0/SceneTreeEditor:0/Tree:0", true,
 			_has_put_kitten_last),
-
+		TutorialStep.new("Properties (1)", 
+			"Netjes!\n\nNu gaan we een aantal veel gebruikte eigenschappen (properties) van Sprite2D bekijken."),
+		TutorialStep.new("Properties (2)", 
+			"Een Sprite2D is een soort Node2D, en daarom heeft Sprite2D de eigenschappen van een Node2D ook.\n\nDe eigenschappen (properties) behorende bij Node2D zijn:\npositie, schaal, rotatie en scheefheid.",
+			null, _unfold_node2d_properties,
+			"Panel:0/VBoxContainer:0/HSplitContainer:0/HSplitContainer:0/HSplitContainer:0/HSplitContainer:0/VSplitContainer:0/TabContainer:0/InspectorDock:0/EditorInspector:0/VBoxContainer:0/EditorInspectorSection:3", false),
+		TutorialStep.new("Scheefheid instellen", 
+			"Je kan elk van deze waardes aanpassen.\nSelecteer de kitten (links in de tree) en verschuif de scheefheid (skew) slider uit de inspecteur naar rechts.",
+			null, Callable(),
+			"Panel:0/VBoxContainer:0/HSplitContainer:0/HSplitContainer:0/HSplitContainer:0/HSplitContainer:0/VSplitContainer:0/TabContainer:0/InspectorDock:0/EditorInspector:0/VBoxContainer:0/EditorInspectorSection:3/VBoxContainer:0/EditorPropertyFloat:1", true,
+			_has_changed_kitten_skew),
+		TutorialStep.new("Scheefheid resetten", 
+			"Pas je iets aan, dan verschijnt de kere-keer-were pijl waarmee je terug de standaard-waarde kan herstellen (resetten).\n\nReset de scheefheid (skew) van de kitten terug naar 0.",
+			null, Callable(),
+			"Panel:0/VBoxContainer:0/HSplitContainer:0/HSplitContainer:0/HSplitContainer:0/HSplitContainer:0/VSplitContainer:0/TabContainer:0/InspectorDock:0/EditorInspector:0/VBoxContainer:0/EditorInspectorSection:3/VBoxContainer:0/EditorPropertyFloat:1", true,
+			_has_reset_kitten_skew),
+		TutorialStep.new("Schaal instellen (1)", 
+			"Voor onze oefening gaan we de kitten wat kleiner maken.\nMet de kitten nog steeds geselecteerd gaan we naar schaal x (Scale) en typen we 0.5 + Enter.\n\nNormaal staat lock ratio (het ketting icoon) aan en wordt y vanzelf mee aangepast.",
+			null, Callable(),
+			"Panel:0/VBoxContainer:0/HSplitContainer:0/HSplitContainer:0/HSplitContainer:0/HSplitContainer:0/VSplitContainer:0/TabContainer:0/InspectorDock:0/EditorInspector:0/VBoxContainer:0/EditorInspectorSection:3/VBoxContainer:0/EditorPropertyVector2:1", true,
+			_has_changed_kitten_scale),
+		TutorialStep.new("Schaal instellen (2)", 
+			"Hetzelfde kan je visueel doen met het schaal gereedschap.\n\nActiveer het schaal gereedschap en klik en sleep op de kitten om die groter/kleiner kleiner te maken.",
+			null, Callable(),
+			"Panel:0/VBoxContainer:0/HSplitContainer:0/HSplitContainer:0/HSplitContainer:0/VBoxContainer:0/VSplitContainer:0/VSplitContainer:0/VBoxContainer:0/PanelContainer:1/VBoxContainer:0/CanvasItemEditor:0/HFlowContainer:0/HBoxContainer:0/Button:3", true,
+			_has_changed_kitten_scale_again),
+		TutorialStep.new("Undo/redo", 
+			"(Bijna) alles wat je aanpast via de editor kan je ongedaan maken (undo).\n\nIn het 'Scene' menu vindt je 'Ongedaan maken' met als sneltoets Ctrl+Z.\nMaak je laatste wijzigingen ongedaan tot de schaal van de kitten terug (0.5, 0.5) is.",
+			null, Callable(),
+			"Panel:0/VBoxContainer:0/EditorTitleBar:0/MenuBar:0", true,
+			_has_changed_kitten_scale),
+		TutorialStep.new("Positie en rotatie", 
+			"Positie en rotatie aanpassen is even makkelijk. Je kan ze visueel slepen in de 2D view, of je kan de waardes aanpassen in de inspecteur.\nVerschuif de kitten naar onder met het verplaats gereedschap, zodat ze niet meer in de lucht zweeft.",
+			null, Callable(),
+			"Panel:0/VBoxContainer:0/HSplitContainer:0/HSplitContainer:0/HSplitContainer:0/VBoxContainer:0/VSplitContainer:0/VSplitContainer:0/VBoxContainer:0/PanelContainer:1/VBoxContainer:0/CanvasItemEditor:0/HFlowContainer:0/HBoxContainer:0/Button:1", true,
+			_has_moved_kitten),
+		TutorialStep.new("Spiegelen", 
+			"Er zijn ook een aantal eigenschappen specifiek voor Sprite2D.\n\nJe kan de sprite bijvoorbeeld spiegelen (flip).\nZet 'flip_h' aan en kijk welk effect dit heeft.",
+			null, _unfold_sprite2d_properties,
+			"Panel:0/VBoxContainer:0/HSplitContainer:0/HSplitContainer:0/HSplitContainer:0/HSplitContainer:0/VSplitContainer:0/TabContainer:0/InspectorDock:0/EditorInspector:0/VBoxContainer:0/EditorInspectorSection:0/VBoxContainer:0/EditorPropertyCheck:1", true,
+			_has_mirrored_kitten),
+		TutorialStep.new("Region", 
+			"Andere eigenschappen specifiek voor Sprite2D gaan we nu niet gebruiken:\n'Region' gebruik je voor spritesheets. Dan heb je een afbeelding waar meerdere sprites tegelijk opstaan. Via een region gebruik je daar maar 1 sprite van."),
+		TutorialStep.new("Animation", 
+			"Sprite2D heeft instellingen voor animatie (Animation), als je alles handmatig, via code, wil doen.\nMaar er is een AnimatedSprite2D die animaties veel makkelijker maakt."),
+		
 		TutorialStep.new("Under construction", 
 			"This tutorial is not finished yet.\n\nYou have reached the end.",
 			preload("res://addons/igt/topics/common/under_construction.png")),
@@ -124,3 +169,29 @@ func _has_put_kitten_last() -> bool:
 	return root_children.size() == 2 and \
 		root_children[0].name.to_lower() == 'background' and \
 		root_children[1].name.to_lower() == 'kitten'
+
+func _unfold_node2d_properties() -> void:
+	var section := EditorSpy.alternative_path_to_node("Panel:0/VBoxContainer:0/HSplitContainer:0/HSplitContainer:0/HSplitContainer:0/HSplitContainer:0/VSplitContainer:0/TabContainer:0/InspectorDock:0/EditorInspector:0/VBoxContainer:0/EditorInspectorSection:3")
+	section.unfold()
+
+func _has_changed_kitten_skew() -> bool:
+	return EditorSpy.get_children_recursive(EditorScript.new().get_editor_interface().get_edited_scene_root()).any(func(node): return node is Sprite2D and node.name.to_lower() == "kitten" and node.skew > 0.0)
+
+func _has_reset_kitten_skew() -> bool:
+	return EditorSpy.get_children_recursive(EditorScript.new().get_editor_interface().get_edited_scene_root()).any(func(node): return node is Sprite2D and node.name.to_lower() == "kitten" and node.skew == 0.0)
+
+func _has_changed_kitten_scale() -> bool:
+	return EditorSpy.get_children_recursive(EditorScript.new().get_editor_interface().get_edited_scene_root()).any(func(node): return node is Sprite2D and node.name.to_lower() == "kitten" and node.scale == Vector2(0.5, 0.5))
+
+func _has_changed_kitten_scale_again() -> bool:
+	return EditorSpy.get_children_recursive(EditorScript.new().get_editor_interface().get_edited_scene_root()).any(func(node): return node is Sprite2D and node.name.to_lower() == "kitten" and node.scale != Vector2(0.5, 0.5))
+
+func _has_moved_kitten() -> bool:
+	return EditorSpy.get_children_recursive(EditorScript.new().get_editor_interface().get_edited_scene_root()).any(func(node): return node is Sprite2D and node.name.to_lower() == "kitten" and node.position.y > 100)
+
+func _has_mirrored_kitten() -> bool:
+	return EditorSpy.get_children_recursive(EditorScript.new().get_editor_interface().get_edited_scene_root()).any(func(node): return node is Sprite2D and node.name.to_lower() == "kitten" and node.flip_h)
+
+func _unfold_sprite2d_properties() -> void:
+	var section := EditorSpy.alternative_path_to_node("Panel:0/VBoxContainer:0/HSplitContainer:0/HSplitContainer:0/HSplitContainer:0/HSplitContainer:0/VSplitContainer:0/TabContainer:0/InspectorDock:0/EditorInspector:0/VBoxContainer:0/EditorInspectorSection:0")
+	section.unfold()
